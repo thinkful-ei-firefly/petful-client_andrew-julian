2e61c4e42ca69a6ee58b175ec3e709f01b278da0
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-10-11 11:10:02 -0500

    one more test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. TESTING. Testing again
+          Edit <code>src/App.js</code> and save to reload. TESTING. Testing again. One more time now.
         </p>
         <a
           className="App-link"
