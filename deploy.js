05333d41086eed95b6fab81ed1cd58093d376f0b
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-10-11 11:18:28 -0500

    deploy method added

---
+++ deploy.js
@@ -0,0 +1,11 @@
+const deployURL = process.env.deployURL
+
+if (deployURL) {
+    console.log(`Making deploy request to ${deployURL}`)
+    fetch(deployURL).then(res => {
+        console.log(res)
+    })
+}
+else {
+    console.error('deployURL not set.')
+}
